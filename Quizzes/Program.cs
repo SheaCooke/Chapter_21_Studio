12c1804bad69d0c463f44e5405e0083b9b178b0f
shea cooke <SheaSCooke@gmail.com>
2021-07-08 09:30:38 -0500

    initial commit

---
+++ Quizzes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Quizzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question test = new Question();
+
+            
+        }
+    }
+}
---
+++ Quizzes/Quiz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzes
+{
+    public class Quiz
+    {
+
+        public List<string> QuestionList { get; set; }
+        public double Grade { get; set; }
+
+        public void AddQuestions() // Call on an instance of a checkbox, true/false or multichoice question to add the text portion to the list 
+        {
+
+        }
+
+        public string RunQuiz() // presents the questions to the user and returns a grade
+        {
+
+            return $"Your grade is {this.Grade}%";
+        }
+
+
+    }
+}
